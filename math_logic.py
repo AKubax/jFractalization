b789f1490d2a378bc6a82453d6225d9c1a426018
AKubax <ovmiovmiovmi@gmail.com>
2019-09-25 07:23:15 +0300

    heatmap
    Seaborn heatmap for visualizing iterated matrices

--- math_logic.py
+++ math_logic.py
@@ -1,11 +1,17 @@
 import cmath
 import math
+import pandas
+import seaborn
+import matplotlib
 
 
 def iterate(f, z0, n):
-    if n == 0: return z0
+    res = z0
+    for i in range(n):
+        res = f(res)
 
-    return iterate(f, f(z0), n - 1)
+    return res
+    
 
 #density - number of dots per 1 linear unit, n - number of iterations
 def constructIteratedMatrix(f, upperLeft, lowerRight, density, n):
@@ -25,4 +31,20 @@ def constructIteratedMatrix(f, upperLeft, lowerRight, density, n):
     return res
         
             
-            
+def heatmapIteratedMatrix(mat, upperLeft, lowerRight, density):
+    modMat = []
+    for i in range(len(mat)):
+        modMat.append([])
+        for j in range(len(mat[i])):
+            modMat[i].append(abs(mat[i][j]))
+        
+    seaborn.heatmap(pandas.DataFrame(modMat, index = [upperLeft.imag - float(i) / density for i in range(int((upperLeft.imag - lowerRight.imag) * density) + 1)], columns = [upperLeft.real + float(i) / density for i in range(int((upperLeft.imag - lowerRight.imag) * density) + 1)]))
+    matplotlib.pyplot.show()
+
+'''
+def func(z):
+	return z * (z - 1) * (z + 1)
+
+res = constructIteratedMatrix(func, complex(-1, 1), complex(1, -1), 10000, 1000)
+heatmapIteratedMatrix(res, complex(-1, 1), complex(1, -1), 100)
+'''
