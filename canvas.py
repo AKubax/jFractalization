c1331b96ee3714c3e116362bcbde5254649d2247
qerased <55762598+qerased@users.noreply.github.com>
2019-09-25 20:46:56 +0300

    Update canvas.py

--- canvas.py
+++ canvas.py
@@ -9,8 +9,9 @@ def draw_fractal(res, canvas):
     for i in range(0, len(res)):
         for j in range(0, len(res[i])):
             if not math.isnan(res[i][j].real):
-                paint_pixel(rescale(i, 0, len(res[i]), 0, 600), rescale(j, 0, len(res[i]), 0, 400), canvas)
+                paint_pixel(rescale(j, 0, len(res[i]), 0, 600), rescale(i, 0, len(res), 0, 400), canvas)
                 
+
 def rescale(value, min_a, max_a, min_b, max_b):
     a = max_a - min_a
     b = max_b - min_b
@@ -22,10 +23,11 @@ def main():
     root.geometry('600x400+0+0')
     root.title("jFractalization")
     root['bg'] = 'white'
-    canvas = Canvas(root, width=600, height=400, bg='white')
-    canvas['bg'] = 'black'
+    root.resizable(False, False)
     
-    Button(text="rand", width=20, command=lambda: paint_pixel(random.randint(0, 600), random.randint(0, 400), canvas)).pack()
+    canvas = Canvas(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight(), bg='white')
+    canvas['bg'] = 'black'
+
     canvas.pack()
     root.mainloop()
 
