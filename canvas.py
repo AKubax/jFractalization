256a0e962d940d03b40d59630a3dd1182a771f43
qerased <55762598+qerased@users.noreply.github.com>
2019-09-27 22:19:39 +0300

    Update canvas.py

--- canvas.py
+++ canvas.py
@@ -5,6 +5,8 @@ from OpenGL.GLUT import *
 import random
 import math
 
+windowWidth = 640
+windowHeight = 480
 
 def paint_pixel(x, y):
     glBegin(GL_POINTS)
@@ -12,11 +14,11 @@ def paint_pixel(x, y):
     glEnd()
 
 
-def draw_fractal(res, canvas):
+def draw_fractal(res):
     for i in range(0, len(res)):
         for j in range(0, len(res[i])):
             if not math.isnan(res[i][j].real):
-                paint_pixel(rescale(j, 0, len(res[i]), 0, 600), rescale(i, 0, len(res), 0, 400), canvas)
+                paint_pixel(rescale(j, 0, len(res[i]), 0, windowWidth), rescale(i, 0, len(res), 0, windowHeight))
                 
 
 def rescale(value, min_a, max_a, min_b, max_b):
@@ -30,29 +32,29 @@ def create_window():
     pass
 
 def iterate():
-    glViewport(0, 0, 500,500)
+    glViewport(0, 0, windowWidth, windowHeight)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
-    glMatrixMode (GL_MODELVIEW)
+    glOrtho(0.0, windowWidth, windowHeight, 0.0, 0.0, 1.0)
+    glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
-def showScreen():
+def showScreen(res = 0):
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
     iterate()
     glColor3f(1.0, 0.0, 3.0)
-    paint_pixel(100, 100)
+    if res != 0:
+        draw_fractal(res)
     glutSwapBuffers()
 
 if __name__ == "__main__":
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
     
-    glutInitWindowSize(600, 480)
+    glutInitWindowSize(windowWidth, windowHeight)
     glutInitWindowPosition(0, 0)
     window = glutCreateWindow(b"jFractalization")
 
     glutDisplayFunc(showScreen)
-    glutIdleFunc(showScreen)
+    glutIdleFunc(showScreen(res))
     glutMainLoop()
